5bd6c061a54c4180ce6f8af0e46fa6338680ec98
Juan Carlos Cuevas Martínez <jccuevas@ujaen.es>
2016-11-15 19:08:23 +0100

    Actividad para buscar dispositivos Bluetooth Fragmento Acerca de Fragmento de Ayuda

---
+++ app/src/main/java/es/ujaen/ejemplostema3/MainActivity.java
@@ -0,0 +1,195 @@
+package es.ujaen.ejemplostema3;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.SubMenu;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+    public static final String FRAGMENTO_DETALLES = "detalles";
+
+    public static final int MENU_CONTEXTUAL_AYUDA = 1;
+
+    FragmentManager mFM = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        mFM = getSupportFragmentManager();
+
+
+        Fragment f = mFM.findFragmentById(R.id.main_container);
+        if (f == null) showHelpFragment();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showHelpFragment();
+
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    public void showHelpFragment() {
+        FragmentTransaction ft = mFM.beginTransaction();
+        FragmentoInfo info = new FragmentoInfo();
+        Fragment f = mFM.findFragmentById(R.id.main_container);
+        if (f != null && !FragmentoInfo.class.isInstance(f)) {
+            ft.remove(f);
+            ft.replace(R.id.main_container, info);
+        } else {
+            ft.add(R.id.main_container, info, "INFO");
+        }
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+
+
+    /**
+     * Se controla el evento de pulsación de la tecla de volver, haciendo que si está abierto el
+     * menú lateral se cierre, y si ya está cerrado se actúe por defecto
+     */
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    /**
+     * Ejemplo de creación de un submenú
+     *
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        boolean result = super.onCreateOptionsMenu(menu);
+        SubMenu fileMenu = menu.addSubMenu("File");
+        fileMenu.add("new");
+        fileMenu.add("open");
+        fileMenu.add("save");
+        return result;
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_help) {
+            showHelpFragment();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_CONTEXTUAL_AYUDA:
+                showHelpFragment();
+                return true;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+
+        if (id == R.id.nav_bluetooth) {//Opción mostrar fragmento de manejo de Bluetooth
+            Intent btactivity= new Intent(this,ActivityBluetooth.class);
+            startActivity(btactivity);
+        }
+
+
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode,
+//                                           String permissions[], int[] grantResults) {
+//        switch (requestCode) {
+//            case REQUEST_EXTERNAL_STORAGE: {
+//                // If request is cancelled, the result arrays are empty.
+//                if (grantResults.length > 0
+//                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//
+//                   showAudioFragment();
+//
+//                } else {
+//                    showAudioFragment();
+//                    Toast.makeText(this, "El ejemplo de audio necesita del permiso para leer", Toast.LENGTH_LONG).show();
+//                }
+//                return;
+//            }
+//
+//            // other 'case' lines to check for other
+//            // permissions this app might request
+//        }
+//    }
+
+
+
+}
